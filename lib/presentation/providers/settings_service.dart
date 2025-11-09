8d586f631d6fef80752af4ba73691937497fcd30
carlo <awiezzz@qq.com>
2025-11-09 18:33:24 +0800

    fix:修正导入代码

--- lib/presentation/providers/settings_service.dart
+++ lib/presentation/providers/settings_service.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../data/services/settings_service.dart';
+import '../../data/services/setting_service.dart';
 
 class SettingsState {
   final String? apiKey;

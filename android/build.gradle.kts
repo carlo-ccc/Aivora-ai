59d5378acefda1a59c74ce9035471b08c5f5c2bc
carlo <awiezzz@qq.com>
2025-12-07 23:54:29 +0800

    fix:更新 Gradle 子项目 build 目录策略避免跨盘路径

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,8 +12,12 @@ val newBuildDir: Directory =
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    val projectPath = project.projectDir.toPath().toAbsolutePath()
+    val rootPath = rootProject.projectDir.toPath().toAbsolutePath()
+    if (projectPath.startsWith(rootPath)) {
+        val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+        project.layout.buildDirectory.value(newSubprojectBuildDir)
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
